dd742f94bb583750ead7610e1b5c6d826ea96f64
SimoneCalzari <calzarisimone@gmail.com>
2024-05-08 18:46:29 +0200

    feat: scaffolding completo

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,15 @@
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      persona: "Simone",
+    };
+  },
+};
 </script>
 
-<template></template>
+<template>
+  <div class="vh-100 d-flex align-items-center">
+    <h1 class="text-center flex-grow-1">Ciao da VUE3, by {{ persona }}</h1>
+  </div>
+</template>
