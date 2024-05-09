701722d243d80813b4b942c09756155370c5c832
SimoneCalzari <calzarisimone@gmail.com>
2024-05-09 20:17:10 +0200

    feat: milestone completate

--- src/components/Converter.vue
+++ src/components/Converter.vue
@@ -15,10 +15,52 @@ export default {
   methods: {
     getCurrencies() {
       axios
-        .get(`${this.store.baseUrlApi}/${this.store.uriCurrencies}`)
+        .get(`${store.baseUrlApi}/${store.uriCurrencies}`)
         .then((response) => {
           this.currenciesList = response.data;
         });
+    },
+    async apiConversion(amount, from, to) {
+      return await axios.get(`${store.baseUrlApi}/${store.latestUri}`, {
+        params: {
+          amount,
+          from,
+          to,
+        },
+      });
+    },
+    conversionValues(fieldToChange) {
+      // ho modificato input 2, voglio cambi il valore dell input 1
+      if (fieldToChange === 1) {
+        if (!store.value2) {
+          store.value1 = "";
+          return;
+        }
+        this.apiConversion(store.value2, store.currency2, store.currency1).then(
+          (response) => {
+            store.value1 = response.data.rates[store.currency1];
+          }
+        );
+        return;
+      }
+      // ho modificato input 1, voglio cambi il valore dell input 2
+      if (!store.value1) {
+        store.value2 = "";
+        return;
+      }
+      this.apiConversion(store.value1, store.currency1, store.currency2).then(
+        (response) => {
+          store.value2 = response.data.rates[store.currency2];
+        }
+      );
+    },
+    conversionValuesOnCurrencyChange() {
+      if (!store.value1) store.value1 = 1;
+      this.apiConversion(store.value1, store.currency1, store.currency2).then(
+        (response) => {
+          store.value2 = response.data.rates[store.currency2];
+        }
+      );
     },
   },
   created() {
@@ -29,7 +71,12 @@ export default {
 
 <template>
   <section>
-    <CurrencyField :currencies="currenciesList" :field="1"></CurrencyField>
-    <CurrencyField :currencies="currenciesList" :filed="2"></CurrencyField>
+    <CurrencyField
+      v-for="index in 2"
+      :currencies="currenciesList"
+      :field="index"
+      @changeVal="conversionValues"
+      @changeCurrency="conversionValuesOnCurrencyChange"
+    ></CurrencyField>
   </section>
 </template>
--- src/components/CurrencyField.vue
+++ src/components/CurrencyField.vue
@@ -10,6 +10,7 @@ export default {
     currencies: Object,
     field: Number,
   },
+  emits: ["changeVal", "changeCurrency"],
 };
 </script>
 
@@ -20,24 +21,42 @@ export default {
       type="number"
       class="form-control text-center"
       v-model="store.value1"
+      @keyup="$emit('changeVal', 2)"
+      @change="$emit('changeVal', 2)"
     />
     <input
       v-else
       type="number"
       class="form-control text-center"
       v-model="store.value2"
+      @keyup="$emit('changeVal', 1)"
+      @change="$emit('changeVal', 1)"
     />
     <select
       v-if="field === 1"
       class="form-select text-center"
       v-model="store.currency1"
+      @change="$emit('changeCurrency')"
     >
-      <option v-for="(value, key) in currencies" :value="key">
+      <option
+        v-for="(value, key) in currencies"
+        :value="key"
+        :disabled="key === store.currency2"
+      >
         {{ value }}
       </option>
     </select>
-    <select v-else class="form-select text-center" v-model="store.currency2">
-      <option v-for="(value, key) in currencies" :value="key">
+    <select
+      v-else
+      class="form-select text-center"
+      v-model="store.currency2"
+      @change="$emit('changeCurrency')"
+    >
+      <option
+        v-for="(value, key) in currencies"
+        :value="key"
+        :disabled="key === store.currency1"
+      >
         {{ value }}
       </option>
     </select>
